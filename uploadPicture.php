e7ea89ddb97944b93b8b3dd95b0a34f24763c869
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-10-23 17:18:20 +0100

    Small modifications

--- uploadPicture.php
+++ uploadPicture.php
@@ -1,3 +1,19 @@
+<script>
+    function confirmationBox(txt) 
+    {
+      var decision = false;
+      if (confirm(txt)) 
+      {
+        decision = true;
+      }
+      else 
+      {
+        decision = false;
+      }
+      document.getElementById("demo").innerHTML = txt;
+    }
+</script>
+
 <?php
 
 session_start();
@@ -5,14 +21,14 @@ session_start();
 $user = $_SESSION['user'] ?? null;
 
 $target_dir = 'id/' . $user["id"] . '/';
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . "profilePicture.png";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["profilePicture"])) 
 {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) 
+    if($check != false) 
     {
         $uploadOk = 1;
     }
@@ -25,8 +41,8 @@ if(isset($_POST["profilePicture"]))
 //Check if the photo already exists - ask the user if they want to replace their old one
 if (file_exists($target_file)) 
 {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+    $txt = "Replace current profile picture?";
+    echo "<script> confirmationBox(",$txt,"); </script>";
 }
 
 // Check file size
@@ -46,11 +62,7 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 // Check if $uploadOk is set to 0 by an error, if not, upload the file
 if ($uploadOk != 0) 
 {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
-    {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    }
-    else 
+    if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
     {
         echo "Sorry, there was an error uploading your file.";
     }
